7263095eccdae3cd6dcf62c06e3ac5a5d2925098
Maxim <foba1995@gmail.com>
2020-11-20 00:59:20 +0400

    empty vue template

--- medods/src/App.vue
+++ medods/src/App.vue
@@ -1,36 +1,23 @@
 <template>
   <div id="app">
-    <Form/>
+
   </div>
 </template>
 
 <script>
-import Form from "./components/Form.vue";
 
 export default {
   name: "App",
-  components: {
-    Form
-  }
+
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-body {
-  margin:  0;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background: rgb(39, 38, 38);
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 </style>
--- medods/src/main.js
+++ medods/src/main.js
@@ -1,9 +1,8 @@
 import Vue from "vue";
-import Vuelidate from "vuelidate";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
-Vue.use(Vuelidate);
+
 
 new Vue({
   render: (h) => h(App),
