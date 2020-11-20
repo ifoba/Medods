1b7eefc85347bb72630b37369a1b4e7d9db56c32
Maxim <foba1995@gmail.com>
2020-11-21 03:46:46 +0400

    init project

--- medods/src/App.vue
+++ medods/src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
-
+    <div class="box">
+      <div class="game-container">
+        <Button v-for="col in color" :key="col" :color="col"/>
+        <div class="center">Simon The Game</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Button from "./components/Button.vue";
 export default {
   name: "App",
-
+  components: {
+    Button,
+  },
+  data: () => {
+    return {
+      color: {
+        first: "red",
+        second: "green",
+        third: "yellow",
+        fourth: "cadetblue",
+      },
+    };
+  },
 };
 </script>
 
@@ -19,5 +36,31 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.box {
+  position: relative;
+  width: 400px;
+  height: 400px;
+}
+.game-container {
+  display: flex;
+  width: 400px;
+  flex-wrap: wrap;
+  border-radius: 30%;
+  border: 2px solid black;
+  overflow: hidden;
+}
+.center {
+  width: 200px;
+  height: 100px;
+  color: black;
+  background: white;
+  border-radius: 40px;
+  position: absolute;
+  top: 150px;
+  left: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ medods/src/components/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="button"
+    :style="{ background: color, opacity: '0.6' }"
+    :class="{ active: press }"
+    @click="pressed"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["color"],
+  data: () => {
+    return {
+      press: false,
+    };
+  },
+  methods: {
+    pressed() {
+      if (this.press) {
+        return;
+      }
+      console.log(this.color);
+      this.press = true;
+      setTimeout(() => {
+        this.press = false;
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style>
+.button {
+  width: 200px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  cursor: pointer;
+}
+.active {
+  opacity: 1!important;
+}
+</style>
